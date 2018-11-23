3211776923bfcdfe374646a2667c10968a143b23
mandylatham <mandylthm@gmail.com>
2018-11-23 18:55:42 -0400

    Fix wrong foreign key used in drop - campaign relationship

--- app/Models/Drop.php
+++ app/Models/Drop.php
@@ -24,14 +24,9 @@ class Drop extends Model
 
     protected $primaryKey = 'id';
 
-    public function getIdAttribute()
-    {
-        return $this->id;
-    }
-
     public function campaign()
     {
-        return $this->belongsTo(Campaign::class, 'campaign_id', 'campaign_id');
+        return $this->belongsTo(Campaign::class, 'campaign_id', 'id');
     }
 
 //    public function recipients()
