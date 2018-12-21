7b7dc12192d11466592c2bd5c198eca89477c5dc
mandylatham <mandylthm@gmail.com>
2018-12-21 14:19:10 -0400

    Add color styles to view campaign sidebar

--- resources/js/components/wrapper/wrapper.component.vue
+++ resources/js/components/wrapper/wrapper.component.vue
@@ -1,16 +1,13 @@
 <template>
     <div class="wrapper-component" :class="{'open': asideOpen, 'has-sidebar': hasSidebar}">
         <div class="wrapper-sidebar-controls">
-            <a href="javascript:;" class="wrapper-sidebar-toggle" @click="asideOpen = !asideOpen">
+            <a href="javascript:;" class="wrapper-sidebar-controls-toggle" @click="asideOpen = !asideOpen">
                 <slot name="sidebar-toggle">
                     <menu-icon></menu-icon>
                 </slot>
             </a>
         </div>
         <div class="wrapper-sidebar">
-            <a href="javascript:;" class="wrapper-sidebar-close-control" @click="asideOpen = false;">
-                <x-icon></x-icon>
-            </a>
             <slot name="wrapper-sidebar"></slot>
         </div>
         <div class="wrapper-content">
--- resources/js/page-components/campaign/view/campaign-view.component.vue
+++ resources/js/page-components/campaign/view/campaign-view.component.vue
@@ -1,23 +1,44 @@
 <template>
     <wrapper class="wrapper--campaign-view" :has-sidebar="true">
         <template slot="wrapper-sidebar">
-            <div class="campaign-avatar"></div>
-            <nav class="wrapper-aside--navigation">
+            <div class="company-data">
+                <div class="company-avatar"></div>
+                <div class="campaign-name">CAMPAIGN NAME</div>
+                <div class="company-address">711 W Harvard ST Orlando, FL 3238, US</div>
+                <div class="company-email">email@profitminer.io</div>
+                <div class="company-phone">333-3333-333</div>
+            </div>
+            <nav class="wrapper-sidebar-navigation">
                 <ul>
                     <li>
-                        <a :class="{'active': activeSection === 'stats'}" href="javascript:;" @click="changePage('stats')">STATS</a>
+                        <a :class="{'active': activeSection === 'stats'}" href="javascript:;" @click="changePage('stats')">
+                            <span class="pm-font-drops-icon"></span>
+                            <span>STATS</span>
+                        </a>
                     </li>
                     <li>
-                        <a :class="{'active': activeSection === 'drops'}" href="javascript:;" @click="changePage('drops')">DROPS</a>
+                        <a :class="{'active': activeSection === 'drops'}" href="javascript:;" @click="changePage('drops')">
+                            <span class="pm-font-drops-icon"></span>
+                            <span>DROPS</span>
+                        </a>
                     </li>
                     <li>
-                        <a :class="{'active': activeSection === 'recipients'}" href="javascript:;" @click="changePage('recipients')">RECIPIENTS</a>
+                        <a :class="{'active': activeSection === 'recipients'}" href="javascript:;" @click="changePage('recipients')">
+                            <span class="pm-font-recipients-icon"></span>
+                            <span>RECIPIENTS</span>
+                        </a>
                     </li>
                     <li>
-                        <a :class="{'active': activeSection === 'responses'}" href="javascript:;" @click="changePage('responses')">RESPONSES</a>
+                        <a :class="{'active': activeSection === 'responses'}" href="javascript:;" @click="changePage('responses')">
+                            <span class="pm-font-responses-icon"></span>
+                            <span>RESPONSES</span>
+                        </a>
                     </li>
                     <li>
-                        <a :class="{'active': activeSection === 'edit'}" href="javascript:;" @click="changePage('edit')">EDIT</a>
+                        <a :class="{'active': activeSection === 'edit'}" href="javascript:;" @click="changePage('edit')">
+                            <span class="pm-font-edit-icon"></span>
+                            <span>EDIT</span>
+                        </a>
                     </li>
                 </ul>
             </nav>
