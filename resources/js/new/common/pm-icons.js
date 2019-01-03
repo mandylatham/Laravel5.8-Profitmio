5c8bea56fc8d36a63fac82a6e9e2962f791c03b4
mandylatham <mandylthm@gmail.com>
2019-01-02 20:25:45 -0400

    Import edit icon from vue-feather-icons

--- resources/js/new/common/pm-icons.js
+++ resources/js/new/common/pm-icons.js
@@ -1,9 +1,10 @@
 import Vue from 'vue';
-import {ChevronLeftIcon, ChevronRightIcon, DownloadIcon, MenuIcon, MoreVerticalIcon, PlusIcon, TrashIcon, XIcon} from 'vue-feather-icons';
+import {ChevronLeftIcon, ChevronRightIcon, DownloadIcon, Edit2Icon, MenuIcon, MoreVerticalIcon, PlusIcon, TrashIcon, XIcon} from 'vue-feather-icons';
 
 Vue.component('chevron-left-icon', ChevronLeftIcon);
 Vue.component('chevron-right-icon', ChevronRightIcon);
 Vue.component('download-icon', DownloadIcon);
+Vue.component('edit-2-icon', Edit2Icon);
 Vue.component('menu-icon', MenuIcon);
 Vue.component('more-vertical-icon', MoreVerticalIcon);
 Vue.component('plus-icon', PlusIcon);
