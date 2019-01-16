eeb5eddbd6d5a814626493df5813c56842a61cf2
mandylatham <mandylthm@gmail.com>
2019-01-16 15:03:48 -0600

    fixed my fix

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,13 +6,9 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\View;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    public function boot()
-    {
-        View::share('loggedUser', auth()->user());
-    }
 }
