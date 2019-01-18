55fc0265dfdb6838db20f6ede633595ed018ab2b
mandylatham <mandylthm@gmail.com>
2019-01-18 01:55:57 -0400

    [wip] Add base layout for campaigns pages, move files from campaign to campaigns folder

---
+++ resources/views/campaigns/base.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.base', [
+    'hasSidebar' => true
+])
+
+@section('sidebar-content')
+    <div class="company-data">
+        <div class="company-data--image" :style="{backgroundImage: 'url(\'' + user.image_url + '\')'}"></div>
+        <h3>{{ $campaign->name }}</h3>
+    </div>
+    <nav class="company-navigation">
+        <ul>
+            <li>
+                <a href="">
+                    <i class="pm-font-stats-icon"></i>
+                    <span>STATS</span>
+                </a>
+            </li>
+            <li>
+                <a class="{{ \Route::current()->getName() === 'campaigns.drops.index' ? 'active' : '' }}" href="{{ route('campaigns.drops.index', ['campaign' => $campaign->id]) }}">
+                    <i class="pm-font-drops-icon"></i>
+                    <span>DROPS</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="pm-font-recipients-icon"></i>
+                    <span>RECIPIENTS</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="pm-font-responses-icon"></i>
+                    <span>RESPONSES</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <i class="pm-font-edit-icon"></i>
+                    <span>EDIT</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+@endsection
