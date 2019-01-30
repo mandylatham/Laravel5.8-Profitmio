34736afce26f37f913cbdfe8778fb03a654878d8
mandylatham <mandylthm@gmail.com>
2019-01-30 16:23:45 -0400

    Add email to recipient resourcew

--- app/Http/Resources/Recipient.php
+++ app/Http/Resources/Recipient.php
@@ -18,6 +18,7 @@ class Recipient extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
+            'email' => $this->email,
             'phone' => $this->phone,
             'address1' => $this->address1,
             'city' => $this->city,
