97c4ce0d19da0f0fe71b1bd88cfe8dc981c925e9
mandylatham <mandylthm@gmail.com>
2019-02-14 10:54:02 -0600

    fixed private media bucket name

--- config/filesystems.php
+++ config/filesystems.php
@@ -60,7 +60,7 @@ return [
             'key' => env('S3_KEY'),
             'secret' => env('S3_SECRET'),
             'region' => env('S3_REGION'),
-            'bucket' => env('S3_MEDIA_BUCKET'),
+            'bucket' => env('PRIVATE_MEDIA_BUCKET'),
         ],
 
         'media_public' => [
