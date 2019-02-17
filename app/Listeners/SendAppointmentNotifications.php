90974e789d4dc8549ee5bacb3854ea9daae272fd
mandylatham <mandylthm@gmail.com>
2019-02-17 17:15:47 -0600

    fixed the fix

--- app/Listeners/SendAppointmentNotifications.php
+++ app/Listeners/SendAppointmentNotifications.php
@@ -5,7 +5,7 @@ namespace App\Listeners;
 use App\Events\AppointmentCreated;
 use App\Models\Appointment;
 use App\Services\CrmService;
-use App\Jobs\LeadNotification;
+use App\Mail\LeadNotification;
 use App\Facades\TwilioClient;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
