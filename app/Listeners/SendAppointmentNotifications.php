00b9c882bdb3b2964a579e680cfb3f14141c1ffb
mandylatham <mandylthm@gmail.com>
2019-02-17 17:13:42 -0600

    added fix for missing use calls

--- app/Listeners/SendAppointmentNotifications.php
+++ app/Listeners/SendAppointmentNotifications.php
@@ -5,6 +5,8 @@ namespace App\Listeners;
 use App\Events\AppointmentCreated;
 use App\Models\Appointment;
 use App\Services\CrmService;
+use App\Jobs\LeadNotification;
+use App\Facades\TwilioClient;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Log\Logger;
