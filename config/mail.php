c2389eb7876f1d03f5750b755f69288d779b7b26
mandylatham <mandylthm@gmail.com>
2019-02-18 21:53:54 +0000

    corrected from email address

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'support@profitminer.io'),
+        'name' => env('MAIL_FROM_NAME', 'Profit Miner'),
     ],
 
     /*
