54de1bc0cccc989ac284b7973912b0ecfe81d08d
mandylatham <mandylthm@gmail.com>
2019-06-17 10:07:59 -0500

    modified deploy script to troubleshoot deploy error

--- .aws/codedeploy/scripts/afterInstall.sh
+++ .aws/codedeploy/scripts/afterInstall.sh
@@ -3,12 +3,7 @@
 export HOME=/root
 export COMPOSER_HOME=/root
 cd  /home/forge/migration-test.profitminer.io
-echo '
-{
-    "github-oauth": {
-        "github.com": "5cd9cd2879e7f6c642c0fdceedfd68b9dc6345fb"
-    }
-}' > auth.json
+echo '{"github-oauth": {"github.com": "5cd9cd2879e7f6c642c0fdceedfd68b9dc6345fb"}}' > auth.json
 cp .env.staging .env
 cp .env.dusk.example .env.dusk
 aws ssm get-parameter --name /Pm/Staging/WebServers/environment --region us-east-1 --query Parameter.Value | sed -e 's/^"//' -e 's/"$//' | awk '{gsub(/\\n/,"\n")}1' >> .env
