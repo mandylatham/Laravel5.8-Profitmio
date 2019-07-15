8cfa1851bd6d6955217a3192213289e7046bb3c9
mandylatham <mandylthm@gmail.com>
2019-07-15 13:23:19 -0500

    added environment awareness to codedeploy scripts

--- .aws/codedeploy/scripts/afterInstall.sh
+++ .aws/codedeploy/scripts/afterInstall.sh
@@ -6,8 +6,20 @@ cd  /home/forge/app.profitminer.io
 echo '{"github-oauth": {"github.com": "5cd9cd2879e7f6c642c0fdceedfd68b9dc6345fb"}}' > auth.json
 cp .env.staging .env
 cp .env.dusk.example .env.dusk
-aws ssm get-parameter --name /Pm/Production/WebServers/environment --region us-east-1 --query Parameter.Value | sed -e 's/^"//' -e 's/"$//' | awk '{gsub(/\\n/,"\n")}1' >> .env
-composer install
+
+# Environment-Specific configuration
+if [[ $CODEDEPLOY_ENVIRONMENT -eq "production" ]]; then
+    aws ssm get-parameter --name /Pm/Production/WebServers/environment --region us-east-1 --query Parameter.Value | sed -e 's/^"//' -e 's/"$//' | awk '{gsub(/\\n/,"\n")}1' >> .env
+elif [[ $CODEDEPLOY_ENVIRONMENT -eq "testing" ]]; then
+    aws ssm get-parameter --name /Pm/Staging/WebServers/environment --region us-east-1 --query Parameter.Value | sed -e 's/^"//' -e 's/"$//' | awk '{gsub(/\\n/,"\n")}1' >> .env
+else
+    echo "This server is misconfigured for code deployment";
+    exit 126;
+fi
+
+composer install --no-dev
 chown forge:forge -R /home/forge/app.profitminer.io
 php artisan key:generate
 php artisan queue:restart
+
+exit 0;
--- .aws/codedeploy/scripts/beforeInstall.sh
+++ .aws/codedeploy/scripts/beforeInstall.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ $CODEDEPLOY_ENVIRONMENT -eq "" ]]; then
+    echo "This server is not configured for CodeDeploy script use";
+    exit 125;
+fi
+
 pip install --upgrade awscli
 if [[ -d /home/forge/app.profitminer.io ]]; then
     rm -R /home/forge/app.profitminer.io
