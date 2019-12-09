6eb802481aa1d8ddd5229aff8c45a502bb4f3ecc
mandylatham <mandylthm@gmail.com>
2019-12-09 00:35:32 -0400

    fix: fix deployment script (#257)

--- .aws/codedeploy/scripts/afterInstall.sh
+++ .aws/codedeploy/scripts/afterInstall.sh
@@ -6,18 +6,18 @@ cd  /home/forge/app.profitminer.io
 echo '{"github-oauth": {"github.com": "5cd9cd2879e7f6c642c0fdceedfd68b9dc6345fb"}}' > auth.json
 cp .env.staging .env
 cp .env.dusk.example .env.dusk
-
 # Environment-Specific configuration
 if [[ $DEPLOYMENT_GROUP_NAME == "production" ]]; then
     aws ssm get-parameter --name /Pm/Production/WebServers/environment --region us-east-1 --query Parameter.Value | sed -e 's/^"//' -e 's/"$//' | awk '{gsub(/\\n/,"\n")}1' >> .env
+    composer install --no-dev
 elif [[ $DEPLOYMENT_GROUP_NAME == "testing" ]]; then
     aws ssm get-parameter --name /Pm/Staging/WebServers/environment --region us-east-1 --query Parameter.Value | sed -e 's/^"//' -e 's/"$//' | awk '{gsub(/\\n/,"\n")}1' >> .env
+    composer install
 else
     echo "This server is misconfigured for code deployment";
     exit 126;
 fi
 
-composer install --no-dev
 chown forge:forge -R /home/forge/app.profitminer.io
 #php artisan key:generate
 php artisan queue:restart
