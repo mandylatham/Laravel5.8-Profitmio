f20e68d601bc5bffd2ff33b64b895be8fcbcb5e8
mandylatham <mandylthm@gmail.com>
2020-01-19 18:29:01 -0600

    corrected checkin point value

--- app/Services/CampaignUserScoreService.php
+++ app/Services/CampaignUserScoreService.php
@@ -45,7 +45,7 @@ class CampaignUserScoreService
         LeadActivity::SENTTOSERVICE => ['points' => 5],
         LeadActivity::ADDEDAPPOINTMENT => ['points' => 10],
         LeadActivity::CALLEDBACK => ['points' => 10],
-        LeadActivity::CHECK_IN => ['points' => 10],
+        LeadActivity::CHECKED_IN => ['points' => 30],
     ];
 
     /** @var array */
