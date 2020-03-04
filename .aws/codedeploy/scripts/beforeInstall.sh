63e2fe39d0d6c20480b5638a42e768e4ced3f69c
mandylatham <mandylthm@gmail.com>
2020-03-03 22:09:04 -0400

    fix: add force to beforeInstall script (#322)

--- .aws/codedeploy/scripts/beforeInstall.sh
+++ .aws/codedeploy/scripts/beforeInstall.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [[ -d /home/profitminer/profitminer ]]; then
-    rm -R /home/profitminer/profitminer
+    rm -rf /home/profitminer/profitminer
 fi
 if [[ -d /home/profitminer/app.profitminer.io && $DEPLOYMENT_GROUP_NAME == "production" ]]; then
-    rm -R /home/profitminer/app.profitminer.io
+    rm -rf /home/profitminer/app.profitminer.io
 fi
 if [[ -d /home/profitminer/testing.profitminer.io && $DEPLOYMENT_GROUP_NAME == "testing" ]]; then
-    rm -R /home/profitminer/testing.profitminer.io
+    rm -rf /home/profitminer/testing.profitminer.io
 fi
