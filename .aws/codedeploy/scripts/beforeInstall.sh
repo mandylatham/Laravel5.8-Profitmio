843606a40cb97e3d320d6882d5f22bfac3c12d7c
mandylatham <mandylthm@gmail.com>
2020-03-03 21:31:54 -0400

    fix: updae beforeInstall script (#321)

--- .aws/codedeploy/scripts/beforeInstall.sh
+++ .aws/codedeploy/scripts/beforeInstall.sh
@@ -3,3 +3,9 @@
 if [[ -d /home/profitminer/profitminer ]]; then
     rm -R /home/profitminer/profitminer
 fi
+if [[ -d /home/profitminer/app.profitminer.io && $DEPLOYMENT_GROUP_NAME == "production" ]]; then
+    rm -R /home/profitminer/app.profitminer.io
+fi
+if [[ -d /home/profitminer/testing.profitminer.io && $DEPLOYMENT_GROUP_NAME == "testing" ]]; then
+    rm -R /home/profitminer/testing.profitminer.io
+fi
