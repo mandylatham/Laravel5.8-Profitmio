88dff1c4fa64c3657ef3bb2a93dd7ba3573f7f6c
mandylatham <mandylthm@gmail.com>
2020-08-11 20:27:29 -0400

    fix: remove drop:send scheduled command (#352)

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('drops:notify')->everyMinute();
-        $schedule->command('drops:send')->everyMinute();
+//        $schedule->command('drops:send')->everyMinute();
         $schedule->command('campaigns:expire')->daily();
     }
 
